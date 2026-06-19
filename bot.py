1277ebb2f05e4382baf88625784b678e5e8eff22
pythonpudge <11111saske@gmail.com>
2026-06-19 20:41:54 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,12 +3,16 @@ import logging
 import sqlite3
 import datetime
 from zoneinfo import ZoneInfo
-from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters import Command
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters import Command
+
+# Настройка логирования
+logging.basicConfig(level=logging.INFO)
 
 # Настройка
 TOKEN = "8805888087:AAEYMCGUWQkWuytZwLCXnAbEz2K6Zd2SyW4"
